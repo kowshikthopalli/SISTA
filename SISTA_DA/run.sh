511ad997e8726616111eeaea381d0e4ffdad1ae5
kowshik thopalli <kthopall@asu.edu>
2022-11-07 17:13:11 -0700

    bash file I used - need to modify

--- SISTA_DA/run.sh
+++ SISTA_DA/run.sh
@@ -1,39 +1,16 @@
-# Table 3        A->D,W
-~/anaconda3/envs/pytorch/bin/python image_source.py --trte val --output ckps/source/ --da uda --gpu_id 0 --dset office --max_epoch 100 --s 0
-~/anaconda3/envs/pytorch/bin/python image_target.py --cls_par 0.3 --da uda --dset office --gpu_id 0 --s 0 --output_src ckps/source/ --output ckps/target/
+# nohup python SHOT/image_NRC_target_Randconv.py --t 1 --gpu_id 0 &
+# nohup python SHOT/image_NRC_target_Randconv.py --t 2 --gpu_id 0 &
+# nohup python SHOT/image_NRC_target_Randconv.py --t 3 --gpu_id 1 &
 
-# Table 4        A->C,P,R
-~/anaconda3/envs/pytorch/bin/python image_source.py --trte val --output ckps/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 50 --s 0
-~/anaconda3/envs/pytorch/bin/python image_target.py --cls_par 0.3 --da uda --dset office-home --gpu_id 0 --s 0 --output_src ckps/source/ --output ckps/target/
-
-# Table 5        VisDA-C
-~/anaconda3/envs/pytorch/bin/python image_source.py --trte val --output ckps/source/ --da uda --gpu_id 0 --dset VISDA-C --net resnet101 --lr 1e-3 --max_epoch 10 --s 0
-~/anaconda3/envs/pytorch/bin/python image_target.py --cls_par 0.3 --da uda --dset VISDA-C --gpu_id 0 --s 0 --output_src ckps/source/ --output ckps/target/ --net resnet101 --lr 1e-3
-
-# Table 7        A->C,P,R (PDA)
-~/anaconda3/envs/pytorch/bin/python image_source.py --trte val --output ckps/source/ --da pda --gpu_id 0 --dset office-home --max_epoch 50 --s 0
-~/anaconda3/envs/pytorch/bin/python image_target.py --cls_par 0.3 --threshold 10 --da pda --dset office-home --gpu_id 0 --s 0 --output_src ckps/source/ --output ckps/target/
-
-# Table 7        A->C,P,R (ODA)
-~/anaconda3/envs/pytorch/bin/python image_source.py --trte val --output ckps/source/ --da oda --gpu_id 0 --dset office-home --max_epoch 50 --s 0
-~/anaconda3/envs/pytorch/bin/python image_target_oda.py --cls_par 0.3 --da oda --dset office-home --gpu_id 0 --s 0 --output_src ckps/source/ --output ckps/target/
-
-
-# Table 8        C,D,W->A (MSDA)
-~/anaconda3/envs/pytorch/bin/python image_source.py --trte val --output ckps/source/ --da uda --gpu_id 0 --dset office-caltech --net resnet101 --max_epoch 100 --s 1
-~/anaconda3/envs/pytorch/bin/python image_source.py --trte val --output ckps/source/ --da uda --gpu_id 0 --dset office-caltech --net resnet101 --max_epoch 100 --s 2
-~/anaconda3/envs/pytorch/bin/python image_source.py --trte val --output ckps/source/ --da uda --gpu_id 0 --dset office-caltech --net resnet101 --max_epoch 100 --s 3
-
-~/anaconda3/envs/pytorch/bin/python image_target.py --cls_par 0.3 --da uda --dset office-caltech --net resnet101 --gpu_id 0 --s 1 --output_src ckps/source/ --output ckps/target/
-~/anaconda3/envs/pytorch/bin/python image_target.py --cls_par 0.3 --da uda --dset office-caltech --net resnet101 --gpu_id 0 --s 2 --output_src ckps/source/ --output ckps/target/
-~/anaconda3/envs/pytorch/bin/python image_target.py --cls_par 0.3 --da uda --dset office-caltech --net resnet101 --gpu_id 0 --s 3 --output_src ckps/source/ --output ckps/target/
-
-~/anaconda3/envs/pytorch/bin/python image_multisource.py --cls_par 0.3 --da uda --dset office-caltech --gpu_id 0 --t 0 --output_src ckps/source/ --output ckps/target/
-
-# Table 8        A->(C,D,W)(MTDA)
-~/anaconda3/envs/pytorch/bin/python image_multitarget.py --cls_par 0.3 --da uda --dset office-caltech --net resnet101 --gpu_id 0 --s 0 --output_src ckps/source/ --output ckps/target/
-
-
-# Table 9       ImageNet->Caltech(PDA)
-~/anaconda3/envs/pytorch/bin/python image_pretrained.py --gpu_id 0 --output ckps/target --cls_par 0.3
-
+# nohup python SHOT/image_NRC_target_feat_mixup.py --t 1 --gpu_id 0 &
+# nohup python SHOT/image_NRC_target_feat_mixup.py --t 2 --gpu_id 0 &
+# nohup python SHOT/image_NRC_target_feat_mixup.py --t 3 --gpu_id 1 &#
+nohup python SHOT/image_NRC_target.py --variant 'direct_target' --gpu_id 8 &
+nohup python SHOT/image_NRC_target.py --variant 'interp_concat' --gpu_id 9 &
+nohup python SHOT/image_NRC_target.py --t 2 --gpu_id 8 &
+nohup python SHOT/image_NRC_target.py --t 3 --gpu_id 9 &
+nohup python SHOT/image_NRC_target.py --train_size 500 --gpu_id 9 &
+nohup python SHOT/image_NRC_target.py --train_size 1000 --gpu_id 5 &
+nohup python SHOT/image_NRC_target.py --train_size 2000 --gpu_id 8 &
+nohup python SHOT/image_NRC_target.py --train_size 5000 --gpu_id 7 &
+nohup python SHOT/image_NRC_target.py --train_size 10000 --gpu_id 6 &
